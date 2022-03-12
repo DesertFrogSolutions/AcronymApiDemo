a1560b8996327214cfa6a354562065c4ae091931
Jonathan Goldfarb <jgoldfar@gmail.com>
2022-03-11 18:55:45 -0800

    Combine live and mocked test files

--- src/index.spec.js
+++ src/index.spec.js
@@ -14,35 +14,50 @@ const assert = chai.assert;
 
 // request testing library
 const request = require('supertest');
-// include server from index.js and the objects we'll mock
-const server = require('./index');
+
+const _live_server = process.env?.npm_config_test_live_server || 'false';
+const live_server = _live_server ? _live_server === 'true' : false;
+
+// include object to be mocked when not using live server
 const { Pool } = require('pg');
+// include server from index.js
+const server = require('./index');
 
 // usage:
 // const t = generateArray();
 // const v = generateArray(10, (n) => {return {value: n};});
-
-function generateArray(length=10, generator = (n) => {return n+1;}) {
-  return Array.from({length: length}, (_, i) => generator(i));
+function generateArray(length = 10, generator = (n) => { return n + 1; }) {
+  return Array.from({ length: length }, (_, i) => generator(i));
 }
 
-describe.only('GET /acronym', () => {
+describe('GET /acronym', () => {
   let poolConnectStub;
   let queryConnectStub;
   beforeEach((done) => {
-    poolConnectStub = sinon.stub(Pool.prototype, 'connect').resolves();
+    if (!live_server) {
+      poolConnectStub = sinon.stub(Pool.prototype, 'connect').resolves();
+    }
     return done();
   });
   afterEach((done) => {
-    poolConnectStub.restore();
-    queryConnectStub.restore();
-    server.close(done);
+    if (!live_server) {
+      try {
+        poolConnectStub.restore();
+        queryConnectStub?.restore();
+      } finally {
+        server.close(done);
+      }
+    } else {
+      done();
+    }
   });
   it('gets a page of acronyms', (done) => {
-    const resultRows = generateArray(10, (n) => {return {acronym_id: n, name: `acronym ${n}`};});
-    queryConnectStub = sinon.stub(Pool.prototype, 'query');
-    queryConnectStub.onCall(0).resolves({rows: [{result_count: 500}]});
-    queryConnectStub.onCall(1).resolves({rows: resultRows});
+    if (!live_server) {
+      const resultRows = generateArray(10, (n) => { return { acronym_id: n, name: `acronym ${n}` }; });
+      queryConnectStub = sinon.stub(Pool.prototype, 'query');
+      queryConnectStub.onCall(0).resolves({ rows: [{ result_count: 500 }] });
+      queryConnectStub.onCall(1).resolves({ rows: resultRows });
+    }
     request(server)
       .get('/acronym')
       .expect(200)
@@ -55,7 +70,7 @@ describe.only('GET /acronym', () => {
           assert.isOk(false, 'no link header');
         }
         assert.isOk(res.body);
-        assert.isAtLeast(res.body.length, resultRows.length);
+        assert.isAtLeast(res.body.length, 1);
         return done();
       }).catch((err) => {
         console.log(err);
@@ -63,10 +78,12 @@ describe.only('GET /acronym', () => {
       });
   });
   it('gets a page of acronyms from 50', (done) => {
-    const resultRows = generateArray(10, (n) => {return {acronym_id: n + 50, name: `acronym ${n + 50}`};});
-    queryConnectStub = sinon.stub(Pool.prototype, 'query');
-    queryConnectStub.onCall(0).resolves({rows: [{result_count: 500}]});
-    queryConnectStub.onCall(1).resolves({rows: resultRows});
+    if (!live_server) {
+      const resultRows = generateArray(10, (n) => { return { acronym_id: n + 50, name: `acronym ${n + 50}` }; });
+      queryConnectStub = sinon.stub(Pool.prototype, 'query');
+      queryConnectStub.onCall(0).resolves({ rows: [{ result_count: 500 }] });
+      queryConnectStub.onCall(1).resolves({ rows: resultRows });
+    }
     request(server)
       .get('/acronym?from=50')
       .expect(200)
@@ -78,18 +95,20 @@ describe.only('GET /acronym', () => {
           assert.isOk(false, 'no link header');
         }
         assert.isOk(res.body);
-        assert.isAtLeast(res.body.length, resultRows.length);
+        assert.isAtLeast(res.body.length, 1);
         return done();
       }).catch((err) => {
         console.log(err);
         done(err);
       });
-    });
+  });
   it('gets a page of acronyms from 50 limit 20', (done) => {
-    const resultRows = generateArray(20, (n) => {return {acronym_id: n + 50, name: `acronym ${n + 50}`};});
-    queryConnectStub = sinon.stub(Pool.prototype, 'query');
-    queryConnectStub.onCall(0).resolves({rows: [{result_count: 500}]});
-    queryConnectStub.onCall(1).resolves({rows: resultRows});
+    if (!live_server) {
+      const resultRows = generateArray(20, (n) => { return { acronym_id: n + 50, name: `acronym ${n + 50}` }; });
+      queryConnectStub = sinon.stub(Pool.prototype, 'query');
+      queryConnectStub.onCall(0).resolves({ rows: [{ result_count: 500 }] });
+      queryConnectStub.onCall(1).resolves({ rows: resultRows });
+    }
     request(server)
       .get('/acronym?from=50&limit=20')
       .expect(200)
@@ -101,7 +120,7 @@ describe.only('GET /acronym', () => {
           assert.isOk(false, 'no link header');
         }
         assert.isOk(res.body);
-        assert.isAtLeast(res.body.length, resultRows.length);
+        assert.isAtLeast(res.body.length, 1);
         return done();
       }).catch((err) => {
         console.log(err);
@@ -109,11 +128,12 @@ describe.only('GET /acronym', () => {
       });
   });
   it('gets a page of acronyms limit 20', (done) => {
-    const resultRows = generateArray(20, (n) => {return {acronym_id: n, name: `acronym ${n}`};});
-    queryConnectStub = sinon.stub(Pool.prototype, 'query');
-    queryConnectStub.onCall(0).resolves({rows: [{result_count: 500}]});
-    queryConnectStub.onCall(1).resolves({rows: resultRows});
-
+    if (!live_server) {
+      const resultRows = generateArray(20, (n) => { return { acronym_id: n, name: `acronym ${n}` }; });
+      queryConnectStub = sinon.stub(Pool.prototype, 'query');
+      queryConnectStub.onCall(0).resolves({ rows: [{ result_count: 500 }] });
+      queryConnectStub.onCall(1).resolves({ rows: resultRows });
+    }
     request(server)
       .get('/acronym?limit=20')
       .expect(200)
@@ -125,7 +145,7 @@ describe.only('GET /acronym', () => {
           assert.isOk(false, 'no link header');
         }
         assert.isOk(res.body);
-        assert.isAtLeast(res.body.length, resultRows.length);
+        assert.isAtLeast(res.body.length, 1);
 
         return done();
       }).catch((err) => {
@@ -134,23 +154,20 @@ describe.only('GET /acronym', () => {
       });
   });
   it('gets a page of acronyms matching the given search', (done) => {
-    const resultRows = generateArray(10, (n) => {return {acronym_id: n, name: `acronym ${n}`};});
-    queryConnectStub = sinon.stub(Pool.prototype, 'query');
-    queryConnectStub.onCall(0).resolves({rows: [{result_count: 500}]});
-    queryConnectStub.onCall(1).resolves({rows: resultRows});
-
+    if (!live_server) {
+      const resultRows = generateArray(10, (n) => { return { acronym_id: n, name: `acronym ${n}` }; });
+      queryConnectStub = sinon.stub(Pool.prototype, 'query');
+      queryConnectStub.onCall(0).resolves({ rows: [{ result_count: 500 }] });
+      queryConnectStub.onCall(1).resolves({ rows: resultRows });
+    }
     request(server)
       .get('/acronym?search=WT')
       .expect(200)
       .then((res) => {
         // do something with response
-        if ('link' in res.headers) {
-          assert.isOk(res.headers.link);
-        } else {
-          assert.isOk(false, 'no link header');
-        }
+        // NOTE - there are <= 20 results for this search with actual data
         assert.isOk(res.body);
-        assert.isAtLeast(res.body.length, resultRows.length);
+        assert.isAtLeast(res.body.length, 1);
 
         return done();
       }).catch((err) => {
@@ -159,23 +176,20 @@ describe.only('GET /acronym', () => {
       });
   });
   it('gets a page of acronyms matching the given search from 2 limit 20', (done) => {
-    const resultRows = generateArray(20, (n) => {return {acronym_id: n + 2, name: `acronym ${n + 2}`};});
-    queryConnectStub = sinon.stub(Pool.prototype, 'query');
-    queryConnectStub.onCall(0).resolves({rows: [{result_count: 500}]});
-    queryConnectStub.onCall(1).resolves({rows: resultRows});
-
+    if (!live_server) {
+      const resultRows = generateArray(20, (n) => { return { acronym_id: n + 2, name: `acronym ${n + 2}` }; });
+      queryConnectStub = sinon.stub(Pool.prototype, 'query');
+      queryConnectStub.onCall(0).resolves({ rows: [{ result_count: 500 }] });
+      queryConnectStub.onCall(1).resolves({ rows: resultRows });
+    }
     request(server)
       .get('/acronym?search=WT&from=2&limit=20')
       .expect(200)
       .then((res) => {
         // do something with response
-        if ('link' in res.headers) {
-          assert.isOk(res.headers.link);
-        } else {
-          assert.isOk(false, 'no link header');
-        }
+        // NOTE - there are <= 20 results for this search with actual data
         assert.isOk(res.body);
-        assert.isAtLeast(res.body.length, resultRows.length);
+        assert.isAtLeast(res.body.length, 1);
 
         return done();
       }).catch((err) => {
@@ -192,19 +206,58 @@ describe('POST /acronym', () => {
 
   // stubs stored into these variables
   let poolConnectStub;
+  let queryConnectStub;
   beforeEach((done) => {
-    poolConnectStub = sinon.stub(Pool.prototype, 'connect').resolves();
+    if (!live_server) {
+      poolConnectStub = sinon.stub(Pool.prototype, 'connect').resolves();
+    }
     return done();
   });
   afterEach((done) => {
-    poolConnectStub.restore();
-    server.close(done);
+    if (!live_server) {
+      try {
+        poolConnectStub.restore();
+        queryConnectStub?.restore();
+      } finally {
+        server.close(done);
+      }
+    } else {
+      done();
+    }
   });
-  it('adds an acronym to the DB', (done) => {
+  it('returns an error status without a name or description', (done) => {
     request(server)
       .post('/acronym')
-      .auth(process.env.API_USER, process.env.API_PASSWORD)
-      .send({name: name, description: description})
+      .send({ description: description })
+      .set('Accept', 'application/json')
+      .expect(400)
+      .then((res) => {
+        console.log(res.body);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    request(server)
+      .post('/acronym')
+      .send({ name: name })
+      .set('Accept', 'application/json')
+      .expect(400)
+      .then((res) => {
+        // console.log(res.body);
+        return done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done(err);
+      });
+  });
+  it('adds an acronym to the DB', (done) => {
+    if (!live_server) {
+      queryConnectStub = sinon.stub(Pool.prototype, 'query').resolves();
+    }
+    request(server)
+      .post('/acronym')
+      .send({ name: name, description: description })
       .set('Accept', 'application/json')
       .expect(200)
       .then((res) => {
@@ -226,18 +279,29 @@ describe('PUT /acronym/:acronym', () => {
 
   // stubs stored into these variables
   let poolConnectStub;
+  let queryConnectStub;
   beforeEach((done) => {
-    poolConnectStub = sinon.stub(Pool.prototype, 'connect').resolves();
+    if (!live_server) {
+      poolConnectStub = sinon.stub(Pool.prototype, 'connect').resolves();
+    }
     return done();
   });
   afterEach((done) => {
-    poolConnectStub.restore();
-    server.close(done);
+    if (!live_server) {
+      try {
+        poolConnectStub.restore();
+        queryConnectStub?.restore();
+      } finally {
+        server.close(done);
+      }
+    } else {
+      done();
+    }
   });
-    it('rejects without authentication', (done) => {
+  it('rejects without authentication', (done) => {
     request(server)
       .put(`/acronym/${oldName}`)
-      .send({name: newName, description: newDescription})
+      .send({ name: newName, description: newDescription })
       .set('Accept', 'application/json')
       .expect(401)
       .then((res) => {
@@ -252,7 +316,7 @@ describe('PUT /acronym/:acronym', () => {
     request(server)
       .put(`/acronym/${oldName}`)
       .auth('fakeuser', 'fakepassword')
-      .send({name: newName, description: newDescription})
+      .send({ name: newName, description: newDescription })
       .set('Accept', 'application/json')
       .expect(401)
       .then((res) => {
@@ -263,18 +327,84 @@ describe('PUT /acronym/:acronym', () => {
         done(err);
       });
   });
+  it('rejects without a name or description', (done) => {
+    request(server)
+      .put('/acronym')
+      .auth(process.env.API_USER, process.env.API_PASSWORD)
+      .send({ description: oldDescription })
+      .set('Accept', 'application/json')
+      .expect(405)
+      .then((res) => {
+        console.log(res.body);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    request(server)
+      .put(`/acronym/${newName}`)
+      .auth(process.env.API_USER, process.env.API_PASSWORD)
+      .send({ bogus: true })
+      .set('Accept', 'application/json')
+      .expect(400)
+      .then((res) => {
+        // do something with response
+        return done();
+      }).catch((err) => {
+        console.log(err);
+        done(err);
+      });
+  });
+  it('adds an acronym to the database', (done) => {
+    if (!live_server) {
+      queryConnectStub = sinon.stub(Pool.prototype, 'query').resolves();
+    }
+    request(server)
+      .put(`/acronym/${newName}`)
+      .auth(process.env.API_USER, process.env.API_PASSWORD)
+      .send({ name: newName, description: newDescription })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((res) => {
+        // do something with response
+        return done();
+      }).catch((err) => {
+        console.log(err);
+        done(err);
+      });
+  });
+  it('adds an acronym to the database without a name in the body', (done) => {
+    if (!live_server) {
+      queryConnectStub = sinon.stub(Pool.prototype, 'query').resolves();
+    }
+    request(server)
+      .put(`/acronym/${newName}`)
+      .auth(process.env.API_USER, process.env.API_PASSWORD)
+      .send({ description: newDescription })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((res) => {
+        // do something with response
+        return done();
+      }).catch((err) => {
+        console.log(err);
+        done(err);
+      });
+  });
   it('updates an acronym in the database', (done) => {
+    if (!live_server) {
+      queryConnectStub = sinon.stub(Pool.prototype, 'query').resolves();
+    }
     request(server)
       .post('/acronym')
       .auth(process.env.API_USER, process.env.API_PASSWORD)
-      .send({name: oldName, description: oldDescription})
+      .send({ name: oldName, description: oldDescription })
       .set('Accept', 'application/json')
       .expect(200);
 
     request(server)
       .put(`/acronym/${oldName}`)
       .auth(process.env.API_USER, process.env.API_PASSWORD)
-      .send({name: newName, description: newDescription})
+      .send({ name: newName, description: newDescription })
       .set('Accept', 'application/json')
       .expect(200)
       .then((res) => {
@@ -294,19 +424,48 @@ describe('DELETE /acronym/:acronym', () => {
 
   // stubs stored into these variables
   let poolConnectStub;
+  let queryConnectStub;
   beforeEach((done) => {
-    poolConnectStub = sinon.stub(Pool.prototype, 'connect').resolves();
+    if (!live_server) {
+      poolConnectStub = sinon.stub(Pool.prototype, 'connect').resolves();
+    }
     return done();
   });
   afterEach((done) => {
-    poolConnectStub.restore();
-    server.close(done);
+    if (!live_server) {
+      try {
+        poolConnectStub.restore();
+        queryConnectStub?.restore();
+      } finally {
+        server.close(done);
+      }
+    } else {
+      done();
+    }
+  });
+  it('rejects without a name', (done) => {
+    request(server)
+      .delete('/acronym')
+      .auth(process.env.API_USER, process.env.API_PASSWORD)
+      .set('Accept', 'application/json')
+      .expect(405)
+      .then((res) => {
+        console.log(res.body);
+        return done();
+      })
+      .catch((err) => {
+        console.log(err);
+        return done(err);
+      });
   });
   it('adds an acronym to the DB and then deletes it', (done) => {
+    if (!live_server) {
+      queryConnectStub = sinon.stub(Pool.prototype, 'query').resolves();
+    }
     request(server)
       .post('/acronym')
       .auth(process.env.API_USER, process.env.API_PASSWORD)
-      .send({name: name, description: description})
+      .send({ name: name, description: description })
       .set('Accept', 'application/json')
       .expect(200);
 
